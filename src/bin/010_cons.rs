8e163f613676d3fdde626d158d109505833e5888
Benjamin Labrecque <labrecque.benji@gmail.com>
2023-12-28 23:11:02 -0500

    Add solution for 10_cons

--- src/bin/010_cons.rs
+++ src/bin/010_cons.rs
@@ -1,3 +1,4 @@
+use rosalind::utils;
 use std::io::{self, Read};
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
@@ -5,22 +6,71 @@ type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 fn main() -> Result<()> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
-    let mut strings: Vec<&str> = vec![];
-    for line in input.lines() {
-        strings.push(line);
-    }
-    let profile_matrix = build_profile_matrix();
-    let consensus_string = build_consensus_string();
+    let dna_strings = utils::read_fasta(input);
+    let profile_matrix = build_profile_matrix(&dna_strings).unwrap();
+    let consensus_string = build_consensus_string(&profile_matrix);
+    println!("{:?}", consensus_string);
+    display_profile_matrix(&profile_matrix);
 
     Ok(())
 }
 
-fn build_profile_matrix() -> Vec<Vec<u32>> {
-    unimplemented!()
+fn build_profile_matrix(dna_strings: &utils::DnaStrings) -> Result<Vec<Vec<u32>>> {
+    let len_dna_strings = dna_strings.values().next().unwrap().len();
+    let mut profile_matrix: Vec<Vec<u32>> = vec![vec![0; len_dna_strings]; 4];
+    for dna_string in dna_strings.values() {
+        for (i, c) in dna_string.chars().enumerate() {
+            match c {
+                'A' => Ok(profile_matrix[0][i] += 1),
+                'C' => Ok(profile_matrix[1][i] += 1),
+                'G' => Ok(profile_matrix[2][i] += 1),
+                'T' => Ok(profile_matrix[3][i] += 1),
+                _ => Err("Found invalid character in DNA string"),
+            }?;
+        }
+    }
+    Ok(profile_matrix)
 }
 
-fn build_consensus_string() -> Vec<Vec<u32>> {
-    unimplemented!()
+fn build_consensus_string(profile_matrix: &Vec<Vec<u32>>) -> String {
+    let len_dna_strings = profile_matrix[0].len();
+    let mut max_occurence: Vec<u32> = vec![0; len_dna_strings];
+    let mut consensus_string: Vec<char> = vec!['A'; len_dna_strings];
+
+    for (row_idx, row) in profile_matrix.iter().enumerate() {
+        for (col_idx, _) in row.iter().enumerate() {
+            if profile_matrix[row_idx][col_idx] > max_occurence[col_idx] {
+                max_occurence[col_idx] = profile_matrix[row_idx][col_idx];
+                consensus_string[col_idx] = row_idx_to_symbol(row_idx);
+            }
+        }
+    }
+    consensus_string.into_iter().collect()
+}
+
+fn display_profile_matrix(profile_matrix: &Vec<Vec<u32>>) {
+    for (row_idx, row) in profile_matrix.iter().enumerate() {
+        let symbol = row_idx_to_symbol(row_idx);
+        println!(
+            "{}: {}",
+            symbol,
+            row.into_iter()
+                .map(|e| e.to_string())
+                .collect::<Vec<String>>()
+                .join(" ")
+        )
+    }
+}
+
+fn row_idx_to_symbol(row_idx: usize) -> char {
+    match row_idx {
+        0 => Ok('A'),
+        1 => Ok('C'),
+        2 => Ok('G'),
+        3 => Ok('T'),
+        _ => Err("Invalid row index"),
+    }
+    .unwrap()
 }
 
 #[cfg(test)]
--- src/utils.rs
+++ src/utils.rs
@@ -1,24 +1,58 @@
-// TODO: What should the return format be?
-//
-// 1. Vec<(id, s)
-// 2. Vec<(DnaString{id: <id>, s: <String>})
-// 3. HashMap<id, s>
+use std::collections::HashMap;
+
+/// <ID, DNA string>
+pub type DnaStrings = HashMap<String, String>;
 
 /// This should take in a file object, or a string, and return the
 /// DNA strings in the file with their corresponding ID.
 ///
-pub fn read_fasta() {
-    unimplemented!()
+pub fn read_fasta(s: String) -> DnaStrings {
+    let mut dna_strings: DnaStrings = HashMap::new();
+    let mut first = true;
+    let mut string_id: &str = "";
+    let mut string_list: Vec<&str> = Vec::new();
+    for line in s.lines() {
+        if line.starts_with(">") {
+            if first {
+                string_id = get_string_id(line);
+                first = false;
+            } else {
+                dna_strings.insert(string_id.into(), string_list.join(""));
+                string_id = get_string_id(line);
+                string_list = Vec::new();
+            }
+        } else {
+            string_list.push(line);
+        }
+    }
+    dna_strings.insert(string_id.into(), string_list.join(""));
+    dna_strings
 }
 
-// TODO: How to create a string with several lines, for testing?
+pub fn get_string_id(s: &str) -> &str {
+    &s[1..]
+}
 
 #[cfg(test)]
 mod tests {
-    use crate::utils::read_fasta;
+    use super::*;
 
     #[test]
     fn read_fasta_simple() {
-        assert_eq!(read_fasta(), "");
+        let input_string: String = "\
+            >Test_123\n\
+            ACGT\n\
+            GTGT\n\
+            >Test_456\n\
+            GGCC\n\
+            CCGG"
+            .to_string();
+        assert_eq!(
+            read_fasta(input_string),
+            HashMap::from([
+                ("Test_123".to_string(), "ACGTGTGT".to_string()),
+                ("Test_456".to_string(), "GGCCCCGG".to_string())
+            ])
+        );
     }
 }
